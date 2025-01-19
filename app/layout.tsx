43e9c443e4dd39599121fa96f2505bc312ce2c5a
Kishan Kumar <krishankumar9943@gmail.com>
2025-01-19 17:47:28 +0530

    RootLayoutChange

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
 import localFont from "next/font/local";
 import { ReactNode } from "react";
-import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
   src: [
-    {
-      path: "./fonts/IBMPlexSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/IBMPlexSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/IBMPlexSans-Semibold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/IBMPlexSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
 const bebasNeue = localFont({
   src: [
-    {
-      path: "./fonts/BebasNeue-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
   ],
   variable: "--bebas-neue",
 });
@@ -45,11 +26,12 @@ const bebasNeue = localFont({
 export const metadata: Metadata = {
   title: "BookWise",
   description:
-    "Bookwise is a book borrowing university library management system.",
+    "BookWise is a book borrowing university library management solution.",
 };
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
+
   return (
     <html lang="en">
       <SessionProvider session={session}>
@@ -57,6 +39,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
           className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
         >
           {children}
+
           <Toaster />
         </body>
       </SessionProvider>
